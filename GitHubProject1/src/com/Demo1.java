8c9ca6ac01fa7384fbe2e0ae82328057042a82db
Dhanshri2228 <166096265+Dhanshri2228@users.noreply.github.com>
2024-07-09 17:37:09 +0530

    by Git

--- GitHubProject1/src/com/Demo1.java
+++ GitHubProject1/src/com/Demo1.java
@@ -5,6 +5,9 @@ public class Demo1 {
 	public void m1() {
 		System.out.println("this is m1----");
 	}
+	public void m1() {
+		System.out.println("this is m1----");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is main method");
