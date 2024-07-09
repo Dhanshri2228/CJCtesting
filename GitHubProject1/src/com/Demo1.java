8a0ad257f4434148297f5f98483b99f3a3130055
ASUS <ASUS@LAPTOP-BUFDU997>
2024-07-09 17:31:10 +0530

    by update

--- GitHubProject1/src/com/Demo1.java
+++ GitHubProject1/src/com/Demo1.java
@@ -1,6 +1,10 @@
 package com;
 
 public class Demo1 {
+	
+	public void m1() {
+		System.out.println("this is m1----");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is main method");
